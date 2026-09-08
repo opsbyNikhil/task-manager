da1174eb858f4b152542a6fbc70f89484d3cdeab
opsbyNikhil <purposejob97@gmail.com>
2026-09-08 23:33:50 +0530

    entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -18,9 +18,9 @@ password = "admin@12345"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(
-        username=username,
+        username=Nikhil,
         email=email,
-        password=password
+        password=Nikhil@12345
     )
     print("Superuser created successfully.")
 else:
